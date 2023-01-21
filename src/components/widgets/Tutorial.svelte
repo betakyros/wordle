492af87a4d6728327dc88ae9ae7ebd04996b5f4e
betakyros <zheng.franklin@gmail.com>
2023-01-21 09:01:01 -0500

    find more mikhads

--- src/components/widgets/Tutorial.svelte
+++ src/components/widgets/Tutorial.svelte
@@ -51,7 +51,7 @@
 	Open the settings menu to see some of the additional features.
 	<br />
 	Written with Svelte, in Typescript by
-	<a href="https://github.com/MikhaD" target="_blank" rel="noreferrer">MikhaD</a>.
+	<a href="https://github.com/betakyros" target="_blank" rel="noreferrer">betakyros</a>.
 </div>
 
 <style lang="scss">
